77ef1d1a8669607965b89a26adbf1cddf3da6cfb
Thao Nguyen <thaogreenhouses@gmail.com>
2021-01-07 17:09:14 +0700

    remove resource + update linur calender + total price

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var imported = document.createElement('script');
+imported.src = 'LunarSolarConverter.js';
+document.head.appendChild(imported);
+
 const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
@@ -6,6 +10,8 @@ const next_mth_element = document.querySelector('.date-picker .dates .month .nex
 const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
 const days_element = document.querySelector('.date-picker .dates .days');
 const body_element = document.querySelector("body");
+const price_element = document.querySelector(".third-div .total-price");
+const lunar_element = document.querySelector(".lunar-div .lunar-cal");
 
 const months = ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'];
 
@@ -25,6 +31,7 @@ selected_date_element.textContent = formatDate(date);
 selected_date_element.dataset.value = selectedDate;
 
 populateDates();
+
 
 // EVENT LISTENERS
 date_picker_element.addEventListener('click', toggleDatePicker);
@@ -94,14 +101,26 @@ function populateDates(e) {
 			selected_date_element.textContent = formatDate(selectedDate);
 			selected_date_element.dataset.value = selectedDate;
 
+			convertLunar(selectedDay, selectedMonth, selectedYear);
 			populateDates();
 		});
-
 		days_element.appendChild(day_element);
+		window.setTimeout(() => convertLunar(selectedDay, selectedMonth, selectedYear), 100);
 	}
 }
 
 // HELPER FUNCTIONS
+function convertLunar(selectedDay, selectedMonth, selectedYear) {
+	var converter = new LunarSolarConverter();
+	var solar = new Solar();
+	solar.solarYear = selectedYear;
+	solar.solarMonth = selectedMonth + 1;
+	solar.solarDay = selectedDay;
+	var lunar = converter.SolarToLunar(solar);
+	var lunarText = lunar.lunarDay + "-" + lunar.lunarMonth + "-" + lunar.lunarYear
+	lunar_element.textContent = lunarText;
+}
+
 function checkEventPathForClass(path, selector) {
 	for (let i = 0; i < path.length; i++) {
 		if (path[i].classList && path[i].classList.contains(selector)) {
@@ -128,23 +147,38 @@ function formatDate(d) {
 }
 
 // dropdown select
-document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
-    this.querySelector('.custom-select').classList.toggle('open');
+document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
+	this.querySelector('.custom-select').classList.toggle('open');
 })
 
 for (const option of document.querySelectorAll(".custom-option")) {
-    option.addEventListener('click', function() {
-        if (!this.classList.contains('selected')) {
-            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
-            this.classList.add('selected');
-            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
-        }
-    })
+	option.addEventListener('click', function () {
+		if (!this.classList.contains('selected')) {
+			this.classList.add('selected');
+		} else {
+			if (!this.classList.contains("default")) {
+				this.classList.remove('selected');
+			} else {
+				alert("Món Buffet này là bắt buộc");
+				return
+			}
+		}
+		var total = 0, count = 0;
+		for (const option of document.querySelectorAll(".custom-option")) {
+			if (option.classList.contains("selected")) {
+				total += parseInt(option.getAttribute('data-value'));
+				count++;
+
+			}
+		}
+		this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = "CHỌN MÓN - đã chọn " + count;
+		price_element.textContent = total;
+	})
 }
 
-window.addEventListener('click', function(e) {
-    const select = document.querySelector('.custom-select')
-    if (!select.contains(e.target)) {
-        select.classList.remove('open');
-    }
+window.addEventListener('click', function (e) {
+	const select = document.querySelector('.custom-select')
+	if (!select.contains(e.target)) {
+		select.classList.remove('open');
+	}
 });
